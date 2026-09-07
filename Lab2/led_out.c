84af4b47920c1ffa5e733f06b7a9ebc7ce35a31a
Giovanni Bedward <bedwardg@lafayette.edu>
2026-09-07 19:12:22 -0400

    Lab 2 code - Inputs and Outputs

--- Lab2/led_out.c
+++ Lab2/led_out.c
@@ -1,13 +1,14 @@
 #include "led_out.h"
 #include "pico/stdlib.h"
+static const uint32_t led_out_pins = 0x000003fc;// GPIO pins 2-9 for LED output
 void led_out_init () {
-const uint32_t led_out_pins = 0x000003fc; // GPIO pins 2-9 for LED output
- gpio_init (led_out_pins);
- gpio_set_dir (led_out_pins, GPIO_OUT);
- 
+    gpio_init_mask(led_out_pins);
+    gpio_set_dir_out_masked(led_out_pins);
 }
+ 
+
 void led_out_write(uint8_t val){
     
-    gpio_put_masked(0x000003fc, val << 2); // Shift value to match GPIO pins 2-9
-    sleep_ms(100); // Delay for 100 milliseconds
+    gpio_put_masked(led_out_pins, val << 2); // Shift value to match GPIO pins 2-9
+    
 }
